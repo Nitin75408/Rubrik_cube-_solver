70faf3114668659dbc11e1badc0c8ca694648e81
Nitin Madaan <nitinmad73@gmail.com>
2026-09-04 11:40:35 +0530

    PROJECT

---
+++ build/CMakeFiles/rubiks_cube_solver.dir/DependInfo.cmake
@@ -0,0 +1,33 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/Users/nitinmadaan/Desktop/rubik_cube-main/Model/RubiksCube.cpp" "CMakeFiles/rubiks_cube_solver.dir/Model/RubiksCube.cpp.o" "gcc" "CMakeFiles/rubiks_cube_solver.dir/Model/RubiksCube.cpp.o.d"
+  "/Users/nitinmadaan/Desktop/rubik_cube-main/Model/RubiksCube1dArray.cpp" "CMakeFiles/rubiks_cube_solver.dir/Model/RubiksCube1dArray.cpp.o" "gcc" "CMakeFiles/rubiks_cube_solver.dir/Model/RubiksCube1dArray.cpp.o.d"
+  "/Users/nitinmadaan/Desktop/rubik_cube-main/Model/RubiksCube3dArray.cpp" "CMakeFiles/rubiks_cube_solver.dir/Model/RubiksCube3dArray.cpp.o" "gcc" "CMakeFiles/rubiks_cube_solver.dir/Model/RubiksCube3dArray.cpp.o.d"
+  "/Users/nitinmadaan/Desktop/rubik_cube-main/Model/RubiksCubeBitboard.cpp" "CMakeFiles/rubiks_cube_solver.dir/Model/RubiksCubeBitboard.cpp.o" "gcc" "CMakeFiles/rubiks_cube_solver.dir/Model/RubiksCubeBitboard.cpp.o.d"
+  "/Users/nitinmadaan/Desktop/rubik_cube-main/PatternDatabases/CornerDBMaker.cpp" "CMakeFiles/rubiks_cube_solver.dir/PatternDatabases/CornerDBMaker.cpp.o" "gcc" "CMakeFiles/rubiks_cube_solver.dir/PatternDatabases/CornerDBMaker.cpp.o.d"
+  "/Users/nitinmadaan/Desktop/rubik_cube-main/PatternDatabases/CornerPatternDatabase.cpp" "CMakeFiles/rubiks_cube_solver.dir/PatternDatabases/CornerPatternDatabase.cpp.o" "gcc" "CMakeFiles/rubiks_cube_solver.dir/PatternDatabases/CornerPatternDatabase.cpp.o.d"
+  "/Users/nitinmadaan/Desktop/rubik_cube-main/PatternDatabases/NibbleArray.cpp" "CMakeFiles/rubiks_cube_solver.dir/PatternDatabases/NibbleArray.cpp.o" "gcc" "CMakeFiles/rubiks_cube_solver.dir/PatternDatabases/NibbleArray.cpp.o.d"
+  "/Users/nitinmadaan/Desktop/rubik_cube-main/PatternDatabases/PatternDatabase.cpp" "CMakeFiles/rubiks_cube_solver.dir/PatternDatabases/PatternDatabase.cpp.o" "gcc" "CMakeFiles/rubiks_cube_solver.dir/PatternDatabases/PatternDatabase.cpp.o.d"
+  "/Users/nitinmadaan/Desktop/rubik_cube-main/PatternDatabases/math.cpp" "CMakeFiles/rubiks_cube_solver.dir/PatternDatabases/math.cpp.o" "gcc" "CMakeFiles/rubiks_cube_solver.dir/PatternDatabases/math.cpp.o.d"
+  "/Users/nitinmadaan/Desktop/rubik_cube-main/Scanner/CubeScanner.cpp" "CMakeFiles/rubiks_cube_solver.dir/Scanner/CubeScanner.cpp.o" "gcc" "CMakeFiles/rubiks_cube_solver.dir/Scanner/CubeScanner.cpp.o.d"
+  "/Users/nitinmadaan/Desktop/rubik_cube-main/main.cpp" "CMakeFiles/rubiks_cube_solver.dir/main.cpp.o" "gcc" "CMakeFiles/rubiks_cube_solver.dir/main.cpp.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
